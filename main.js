c63abae9a001ba86ca51ce91eba4600f3e408280
AR ARIF <ar.arif7497.1@gmail.com>
2020-06-19 16:04:22 -0700

    code formated

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-async function fetchData() {
+// api fetching
+fetchData = async () => {
   let response = await fetch("https://api.covid19api.com/summary");
   let data = await response.json();
-
   return data;
-}
+};
 
+// calling fetch function
 fetchData().then((data) => afterThat(data));
 
+// main code in this 1 function
 function afterThat(data) {
   const coronaData = data;
   let bangladesh = coronaData.Countries[13];
-
   console.log(bangladesh);
 
   //  object data
@@ -29,39 +30,38 @@ function afterThat(data) {
   let total_deaths = document.getElementById("Total-deaths");
   let total_recovered = document.getElementById("Total-recovered");
 
-  // edit value of corona updRate
-  cases_24.innerText = todayCases;
-
-  deaths_24.innerText = new Intl.NumberFormat("en-US", {
+  // number to currency format
+  let currencyFormat = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 0,
-  }).format(todayRecovered);
+  });
 
-  recovered_24.innerText = todayRecovered;
+  // edit value of corona update
+  cases_24.innerText = currencyFormat.format(todayCases);
+  deaths_24.innerText = currencyFormat.format(todayDeaths);
+  recovered_24.innerText = currencyFormat.format(todayRecovered);
+  total_cases.innerText = currencyFormat.format(totalCases);
+  total_deaths.innerText = currencyFormat.format(totalDeaths);
+  total_recovered.innerText = currencyFormat.format(totalRecovered);
 
-  total_cases.innerText = totalCases;
-  total_deaths.innerText = totalDeaths;
-  total_recovered.innerText = totalRecovered;
+  // remove $
+  dollerRemove = (input) => {
+    let slice = input.innerText.slice(1);
+    input.innerText = slice;
+  };
+  dollerRemove(cases_24);
+  dollerRemove(deaths_24);
+  dollerRemove(recovered_24);
+  dollerRemove(total_cases);
+  dollerRemove(total_deaths);
+  dollerRemove(total_recovered);
 
   // Date
   let date = document.getElementById("date");
-
   const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format();
   const mo = new Intl.DateTimeFormat("en", { month: "short" }).format();
   const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format();
   const currentDate = `${da}-${mo}-${ye}`;
-
   date.innerText = currentDate;
-
-  
-        let slice = deaths_24.innerText.slice(1);
-
-        deaths_24.innerText = slice;
-
-
-   
-  
 }
-
-
