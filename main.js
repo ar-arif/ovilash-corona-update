b1d7650edf41d149b1d2410d7f276b363afd4b55
AR <ar.arif7497.1@gmail.com>
2020-08-04 15:19:03 +0600

    api change

--- main.js
+++ main.js
@@ -1,61 +1,28 @@
-// api fetching
-fetchData = async () => {
-  let response = await fetch("https://api.covid19api.com/summary");
-  let data = await response.json();
-  return data;
-};
+// data fetch from api
+fetch("https://corona-api.herokuapp.com/")
+    .then((apiData) => {
+      return apiData.json();
+    })
+    .then((data) => {
+      afterThat(data);
+    });
 
-// calling fetch function
-fetchData().then((data) => afterThat(data));
 
-// main code in this 1 function
+// main code in this function
 function afterThat(data) {
-  const coronaData = data;
-  let bangladesh = coronaData.Countries[13];
-  console.log(bangladesh);
+  // console.log(data);
 
-  //  object data
-  let todayCases = bangladesh.NewConfirmed;
-  let todayDeaths = bangladesh.NewDeaths;
-  let todayRecovered = bangladesh.NewRecovered;
-  let totalCases = bangladesh.TotalConfirmed;
-  let totalDeaths = bangladesh.TotalDeaths;
-  let totalRecovered = bangladesh.TotalRecovered;
+  const bd = data.BD
 
-  //  html div collect
-  let cases_24 = document.getElementById("cases-24");
-  let deaths_24 = document.getElementById("deaths-24");
-  let recovered_24 = document.getElementById("recovered-24");
-  let total_cases = document.getElementById("Total-cases");
-  let total_deaths = document.getElementById("Total-deaths");
-  let total_recovered = document.getElementById("Total-recovered");
 
-  // number to currency format
-  let currencyFormat = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 0,
-  });
+  // update covid-19 value
+  document.getElementById("cases-24").innerText = bd.new_cases
+  document.getElementById("deaths-24").innerText = bd.new_deaths
+  document.getElementById("recovered-24").innerText = bd.new_recovered
+  document.getElementById("Total-cases").innerText = bd.total_cases
+  document.getElementById("Total-deaths").innerText = bd.total_deaths
+  document.getElementById("Total-recovered").innerText = bd.total_recovered
 
-  // edit value of corona update
-  cases_24.innerText = currencyFormat.format(todayCases);
-  deaths_24.innerText = currencyFormat.format(todayDeaths);
-  recovered_24.innerText = currencyFormat.format(todayRecovered);
-  total_cases.innerText = currencyFormat.format(totalCases);
-  total_deaths.innerText = currencyFormat.format(totalDeaths);
-  total_recovered.innerText = currencyFormat.format(totalRecovered);
-
-  // remove $
-  dollerRemove = (input) => {
-    let slice = input.innerText.slice(1);
-    input.innerText = slice;
-  };
-  dollerRemove(cases_24);
-  dollerRemove(deaths_24);
-  dollerRemove(recovered_24);
-  dollerRemove(total_cases);
-  dollerRemove(total_deaths);
-  dollerRemove(total_recovered);
 
   // Date
   let date = document.getElementById("date");
